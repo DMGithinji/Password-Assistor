f103e37ccd38a19941dfc4ea836466ba4ad7ff96
DMG <dmwaigithinji@gmail.com>
2019-06-14 16:14:15 +0300

    Add test to check whether object is properly initialized

--- user_test.py
+++ user_test.py
@@ -1,6 +1,5 @@
 import unittest
 from user import User
-from credentials import Credentials
 
 class TestUser(unittest.TestCase):
 
@@ -17,6 +16,17 @@ def setUp():
 
     '''
     self.new_contact = Contact("Denis","Githinji","DMG", '!q1@w2#e3$r4%t5', "dmg@gmail.com")
+
+def test_init(self):
+    '''
+    Test to check if if the object is initialized properly
+
+    '''
+    self.assertEqual(self.user.first_name,"Denis")
+    self.assertEqual(self.user.last_name,"Githinji")
+    self.assertEqual(self.user.preferred_username,"DMG")
+    self.assertEqual(self.user.password,"!q1@w2#e3$r4%t5")
+    self.assertEqual(self.user.email,"dmg@gmail.com")
 
 if __name__ == "__main__":
     unittest.main()
