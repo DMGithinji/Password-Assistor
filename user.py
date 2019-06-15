ebb1016dbb8da778ddb5d8c7db236ae8126464f5
DMG <dmwaigithinji@gmail.com>
2019-06-15 17:14:20 +0300

    Add tear down method to clean up data after each test case has run

--- user.py
+++ user.py
@@ -5,14 +5,14 @@ class User:
     
     '''
     user_list = []
-    def __init__ (self, first_name, last_name, preferred_username, password, email):
+    
+    def __init__ (self, first_name, last_name, password, email):
         '''
         __init__ method that helps to define properties for our objects
 
         '''
         self.first_name = first_name
         self.last_name = last_name
-        self.preferred_username = preferred_username
         self.password = password
         self.email = email
 
--- user_test.py
+++ user_test.py
@@ -15,7 +15,14 @@ class TestUser(unittest.TestCase):
         Setup method to run before each test cases.
 
         '''
-        self.new_user = User("Denis","Githinji","DMG", '!q1@w2#e3$r4%t5', "dmg@gmail.com")
+        self.new_user = User("Denis","Githinji", '!q1@w2#e3$r4%t5', "dmg@gmail.com")
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+
 
     def test_init(self):
         '''
@@ -24,7 +31,6 @@ class TestUser(unittest.TestCase):
         '''
         self.assertEqual(self.new_user.first_name,"Denis")
         self.assertEqual(self.new_user.last_name,"Githinji")
-        self.assertEqual(self.new_user.preferred_username,"DMG")
         self.assertEqual(self.new_user.password,"!q1@w2#e3$r4%t5")
         self.assertEqual(self.new_user.email,"dmg@gmail.com")
 
@@ -35,6 +41,16 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user.save_user()
         self.assertEqual(len(User.user_list), 1)
+
+    def test_save_multiple_user(self):
+        '''
+        Test to check if multiple users can be saved
+
+        '''
+        self.new_user.save_user()
+        test_user = User("Test", "User", "12345", "testuser@gmail.com")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
 
 if __name__ == "__main__":
     unittest.main()
