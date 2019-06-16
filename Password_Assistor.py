017c8b79e3202658c25dca210a8fe81ae4edde54
DMG <dmwaigithinji@gmail.com>
2019-06-16 11:39:25 +0300

    Add functionality to add new user account in main file

--- Password_Assistor.py
+++ Password_Assistor.py
@@ -31,8 +31,45 @@ def name_exists(first_name, last_name):
     '''
     return Credentials.name_exists(first_name, last_name)
 
- print("The login details you entered do not march, please try again by entering shortcode 'lg' or exit by entering shortcode 'ex'")
-'''
+while True:
+    print("WELCOME TO YOUR PA - PASSWORD ASSISTOR ")
+    print("Use these short codes to navigate through this section of the app:\nca - create a new user account\nlg - login to existing account\nex -exit the contact list ")
+    print("Input shortcode: ")
+
+    short_code = input().lower()
+
+    if short_code == 'ca':
+        print("New User Account")
+        print("-"*10)
+
+        print ("First name ....")
+        first_name = input()
+
+        print("Last name ...")
+        last_name = input()
+
+        print("Email ...")
+        email = input()
+
+        while True:
+            print("Password ...")
+            password1 = input()
+
+            print("Confirm password ...")
+            password2 = input()
+
+            if password1 == password2:
+                password = password1
+                save_user(create_user(first_name, last_name, email, password))
+                print ('\n')
+                print(f"{first_name} {last_name}, your Password Assistor (PA) account has been successfully been created")
+                print ('\n')
+                break
+
+            else:
+                print("Oops, the passwords do not seem to march, please try again")
+
+
 
 if __name__ == '__main__':
     main()
